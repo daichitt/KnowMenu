45af96ab6e322b7731b21bb9b0558b5fcd20695e
daichit <gene.sixteen.000925@outlook.jp>
2026-06-18 17:25:07 +0700

    add(mobile): layout

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { LocaleEnum, useLocale } from '@/lib/Locale';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const locale = useLocale();
 
   return (
     <Tabs
@@ -19,14 +20,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: locale === LocaleEnum.en ? 'Home' : 'ホーム',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: locale === LocaleEnum.en ? 'Explore' : '探索',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
