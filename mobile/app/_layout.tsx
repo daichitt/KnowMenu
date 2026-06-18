348d5eb097dbb0830b22fd48f5bb01cd9cd3634d
daichit <gene.sixteen.000925@outlook.jp>
2026-06-18 17:22:54 +0700

    add(mobile): setting tab

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -4,20 +4,37 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { LocaleEnum, LocaleProvider, useLocale } from '@/lib/Locale';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+function RootStack() {
+  const locale = useLocale();
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="settings"
+        options={{
+          title: locale === LocaleEnum.en ? 'Settings' : '設定',
+        }}
+      />
+    </Stack>
+  );
+}
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <LocaleProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <RootStack />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </LocaleProvider>
   );
 }
