4609650860d7e18ce6e5006ab8bef8c84fe60d65
daichit <gene.sixteen.000925@outlook.jp>
2026-06-18 17:40:33 +0700

    add(mobile): static page

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -1,17 +1,27 @@
 import { StyleSheet } from 'react-native';
 
+import { Header } from '@/components/Header';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import Locale from '@/lib/Locale';
 
 export default function ExploreScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText>Hello World</ThemedText>
+    <ThemedView style={styles.screen}>
+      <Header />
+      <ThemedView style={styles.container}>
+        <ThemedText>
+          <Locale ja="ハローワールド">Hello World</Locale>
+        </ThemedText>
+      </ThemedView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
